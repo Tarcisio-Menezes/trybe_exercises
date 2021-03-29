df62fe2fa89823e85d8b9a503bca85663cc1671c
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-03-29 15:08:09 -0300

    Exercicio números primos

--- bloco_4/dia_3/exercises.js
+++ bloco_4/dia_3/exercises.js
@@ -37,7 +37,7 @@ else {
 //Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
 //Dica: você vai precisar fazer um loop que vá de 0 ao número definido; Além disso, vai precisar fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
 
-let n = 11;
+let n = 13;
 let score = 0;
 let score_impar = 0;
 
