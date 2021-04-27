961fb4c6469a5bb1ca0017305711a417acf91fba
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-04-27 20:29:06 -0300

    exercicios do dia quase terminados

---
+++ bloco_7/dia_2/exercicioPosAula2.js
@@ -0,0 +1,59 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  // Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+  const addTurno = () => {
+    lesson2.turno = 'manhã';
+  }
+
+  addTurno();
+
+  //Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+  const listKeys = (object) => {
+    const keys = Object.keys(object);
+    return keys;
+  }
+
+  listKeys(lesson2)
+
+  //Crie uma função para mostrar o tamanho de um objeto
+  const objectSize = (object) => {
+    const keys = Object.keys(object);
+    const size = keys.length;
+    return `O objeto tem ${size} chaves`;
+  }
+
+  objectSize(lesson2);
+
+  //Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+  const valuesObject = (object) => {
+    const values = Object.values(object);
+    return values;
+  }
+
+  valuesObject(lesson2);
+
+  //Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+  const agrup = () => {
+    const allLessons = Object.assign({}, lesson1, lesson2, lesson3);
+    return allLessons;
+  }
+
+  console.table(agrup());
