7d437cbedf67bec71dbde98a3980d06dfbb5a4ab
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-05-03 21:45:14 -0300

    exercicio 7 corrigido

--- bloco_8/dia_2/exercise7.js
+++ bloco_8/dia_2/exercise7.js
@@ -68,9 +68,10 @@ const expectedResult = false;
 function authorUnique() {
   // escreva seu código aqui
   return books.every((book) =>
-    !books.some((bookSome) =>
+    books.some((bookSome) =>
       (bookSome.author.birthYear === book.author.birthYear)
       && (bookSome.author.name !== book.author.name)));
 }
 
 assert.strictEqual(authorUnique(), expectedResult);
+console.log(authorUnique());
