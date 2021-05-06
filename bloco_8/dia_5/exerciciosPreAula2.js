eeb73317f89a9dbc6dc06bb4e980efef50c6b917
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-05-06 15:08:24 -0300

    usando o operador spread em parametros de funçao

---
+++ bloco_8/dia_5/exerciciosPreAula2.js
@@ -0,0 +1,17 @@
+const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const months = [...summer, ...fall, ...winter, ...spring];
+console.log(months); /* [
+  'JAN', 'FEV', 'MAR',
+  'ABR', 'MAI', 'JUN',
+  'JUL', 'AGO', 'SET',
+  'OUT', 'NOV', 'DEZ'
+] */
+
+const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
+const patientInfo = [60, 1.7];
+
+console.log(imc(...patientInfo)); // 20.76
