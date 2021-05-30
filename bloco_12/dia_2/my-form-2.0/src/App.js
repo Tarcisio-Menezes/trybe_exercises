2d64ef9ab7b9cbdbc1de4c6e6e988fa2cd82f049
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-05-30 20:24:20 -0300

    faltando apenas mostrar a div ccm dados capturados

--- bloco_12/dia_2/my-form-2.0/src/App.js
+++ bloco_12/dia_2/my-form-2.0/src/App.js
@@ -5,6 +5,13 @@ import Email from './Email';
 import Cpf from './Cpf';
 import End from './End';
 import City from './City';
+import State from './State';
+import Type from './Type';
+import LastJob from './LastJob';
+import Office from './Office';
+import JobDescription from './JobDescription';
+import SucessButton from './SucessButton';
+import DisplayData from './DisplayData'
 
 class App extends React.Component {
 
@@ -14,14 +21,21 @@ class App extends React.Component {
     this.readForm = this.readForm.bind(this);
     this.validEnd = this.validEnd.bind(this);
     this.validCity = this.validCity.bind(this);
-
+    this.officeAlert = this.officeAlert.bind(this);
+    this.showData = this.showData.bind(this);
+    
     this.state = {
       name: '',
       email: '',
       cpf: '',
       end: '',
       city: '',
-    
+      state: '',
+      type: '',
+      jobs: '',
+      office: '',
+      mouseEnter: false,
+      jobDescription: '',
     };
   }
 
@@ -46,17 +60,55 @@ class App extends React.Component {
     });
   }
 
+  officeAlert() {
+    if(!this.state.mouseEnter) {
+      alert('Preencha com cuidado esta informação.');
+
+      this.setState({
+        mouseEnter: true,
+      });
+    };
+  }
+
+  showData() {
+    document.getElementsByClassName('personal')[0].remove();
+  }
+
   render() {
+ 
     return (
-      <div className="App">
+      <div className="personal">
   
       <h1 className="title">Cadastre seu currículo conosco</h1>
 
+      <form action="" method="get">
+
       <Name value={this.state.name} readForm={this.readForm} />
+      <br></br>
       <Email value={this.state.email} readForm={this.readForm}/>
+      <br></br>
       <Cpf value={this.state.cpf} readForm={this.readForm} />
+      <br></br>
       <End value={this.state.end} readForm={this.readForm}validEnd={this.validEnd} />
+      <br></br>
       <City value={this.state.city} readForm={this.readForm} validCity={this.validCity}/>
+      <br></br>
+      <State value={this.state.state} readForm={this.readForm}/>
+      <br></br>
+      <Type value={this.state.type} readForm={this.readForm} />
+      <br></br>
+      <LastJob value={this.state.jobs} readForm={this.readForm}/>
+      <br></br>
+      <Office value={this.state.office} readForm={this.readForm} officeAlert={this.officeAlert} />
+      <br></br>
+      <JobDescription value={this.state.jobDescription} readForm={this.readForm} />
+      <br></br>
+      <div className="buttons">
+      <SucessButton showData={this.showData} />
+      <span> <DisplayData /></span>
+      </div>
+
+      </form>
         
       </div>
     );
