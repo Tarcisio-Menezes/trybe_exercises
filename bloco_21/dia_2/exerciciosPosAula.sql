d0da4bb5368c825402e4013b25d91171f7ae6567
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-08-26 12:47:51 -0300

    adiciona exercicios pos aula 1 ~ 3

---
+++ bloco_21/dia_2/exerciciosPosAula.sql
@@ -0,0 +1,14 @@
+-- Exercício 1: Utilizando o INNER JOIN , encontre as vendas nacionais ( domestic_sales ) 
+-- e internacionais ( international_sales ) de cada filme.
+SELECT m.title, b.domestic_sales, b.international_sales FROM Pixar.Movies AS m
+INNER JOIN Pixar.BoxOffice AS b
+ON b.movie_id = m.id;
+
+
+-- Exercício 2: Utilizando o INNER JOIN , faça uma busca que retorne o número de vendas 
+-- para cada filme que possui um número maior de vendas internacionais ( international_sales ) 
+-- do que vendas nacionais ( domestic_sales ).
+SELECT m.title, b.domestic_sales, b.international_sales FROM Pixar.Movies AS m
+INNER JOIN Pixar.BoxOffice AS b
+ON b.movie_id = m.id
+WHERE b.international_sales > b.domestic_sales;
