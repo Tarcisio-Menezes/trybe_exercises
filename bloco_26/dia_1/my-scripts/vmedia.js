60a50e76a6b51ab3515a385f0eae7d672ab8cb74
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-09-24 01:05:32 -0300

    corrige texto em vmedia

--- bloco_26/dia_1/my-scripts/vmedia.js
+++ bloco_26/dia_1/my-scripts/vmedia.js
@@ -2,6 +2,6 @@ const { questionFloat } = require('readline-sync');
 const { vMedia } = require('./functions');
 
 const distancia = questionFloat('Olá! Para iniciarmos diga a distancia percorrida em metros ');
-const tempo = questionFloat('Estamos indo bem! Para finalizar, nos diga o tempo gasto para percorrer a distância informada ');
+const tempo = questionFloat('Estamos indo bem! Para finalizar, nos diga o tempo gasto, em segundos, para percorrer a distância informada ');
 console.log('OK');
 console.log(`Sua velocidade média é de ${vMedia(distancia, tempo)} m/s^2`);
