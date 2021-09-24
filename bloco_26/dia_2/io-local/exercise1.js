f28cf766a85d0cdc1621869085e53af330490302
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-09-24 13:06:36 -0300

    exercicio 1 && 2 completos

--- bloco_26/dia_2/io-local/exercise1.js
+++ bloco_26/dia_2/io-local/exercise1.js
@@ -1,22 +1,21 @@
-const { questionFloat } = require('readline-sync');
 const { calc } = require('./functions');
 
-const n1 = questionFloat('Entre com o primeiro valor: ');
-const n2 = questionFloat('Entre com o segundo valor: ');
-const n3 = questionFloat('Entre com o terceiro valor: ');
-
-const calculator = new Promise((resolve, reject) => {
-  if (typeof (n1) !== 'number' || typeof (n2) !== 'number' || typeof (n3) !== 'number') {
-    reject (new Error('Entre apenas com valores numéricos'));
-  };
-
-  const result = calc(n1, n2, n3);
-
-  if (result < 50 ) reject (new Error('Valor muito baixo'));
-  else {
-    resolve(result);
-    return console.log(`O resultado de (n1 + n2) * n3 é ${result}`);
-  };
+const calculator = (n1, n2, n3) => {
+  const promiseCalculator = new Promise((resolve, reject) => {
+    if (typeof (n1) !== 'number' || typeof (n2) !== 'number' || typeof (n3) !== 'number') {
+      reject (new Error('Entre apenas com valores numéricos'));
+    };
   
-  return calculator;
-});
+    const result = calc(n1, n2, n3);
+  
+    if (result < 50 ) reject (new Error('Valor muito baixo'));
+    else {
+      resolve(result);
+    };
+
+  });
+    
+  return promiseCalculator;
+}
+
+module.exports = { calculator };
---
+++ bloco_26/dia_2/io-local/exercise2.js
@@ -0,0 +1,9 @@
+const { calculator } = require('./exercise1');
+
+const random1 = Math.round(Math.random() * (100 - 1) + 1, 0);
+const random2 = Math.round(Math.random() * (100 - 1) + 1, 0);
+const random3 = Math.round(Math.random() * (100 - 1) + 1, 0);
+
+calculator(random1, random2, random3)
+  .then(result => console.log(`O resultado de (n1 + n2) * n3 é ${result}`))
+  .catch(error => console.log(`Algo deu errado, erro: ${error}`));
