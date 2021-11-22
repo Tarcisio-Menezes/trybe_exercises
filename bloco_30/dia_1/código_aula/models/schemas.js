6293b9e2d1e9185c2e0251974530539eb340bb07
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-22 18:49:02 -0300

    adiciona codigo da aula do Frank

---
+++ bloco_30/dia_1/código_aula/models/schemas.js
@@ -0,0 +1,18 @@
+const connection1 = {
+  host: "localhost",
+  user: "root" /* Se necessário, altere o user */,
+  password: "secret" /* Não se esqueça de inserir a senha aqui! */,
+  database: "solid_example",
+};
+
+const connection2 = {
+  host: "localhost",
+  user: "root" /* Se necessário, altere o user */,
+  password: "secret" /* Não se esqueça de inserir a senha aqui! */,
+  database: "solid_example_2",
+};
+
+module.exports = {
+  connection1,
+  connection2,
+};
