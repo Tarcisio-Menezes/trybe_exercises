4efcf3e547a6166b776dd4a271a1b6add4ea887a
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-23 12:13:15 -0300

    adiciona regra de negocio em servicePlants

--- bloco_30/dia_1/exercise/src/services/plantsService.js
+++ bloco_30/dia_1/exercise/src/services/plantsService.js
@@ -1,6 +1,29 @@
 const plantsModel = require('../models/plantsModel');
 
-const registerPlant = async (plant) => plantsModel.plantRegister(plant);
+const rate = 0.77;
+const rateCountryBrazil = 8;
+const rateCountryNotBrazil = 7;
+const divisorRate = 2;
+const rateMultiply = 1.33;
+
+const registerPlant = async (plant) => {
+  const { id, breed, needsSun, origin, specialCare, size } = plant;
+  const rateCountry = (origin === 'Brazil') ? rateCountryBrazil : rateCountryNotBrazil;
+
+  const newPlant = {
+    id, breed, needsSun, origin, specialCare, size,
+  };
+
+  if (needsSun) {
+    const waterFrequency = (size * rate) + rateCountry;
+    newPlant.specialCare.waterFrequency = waterFrequency;
+  } else {
+    const waterFrequency = ((size / divisorRate) * rateMultiply) + rateCountry;
+    newPlant.specialCare.waterFrequency = waterFrequency;
+  }
+
+  return plantsModel.plantRegister(newPlant);
+};
 
 module.export = {
   registerPlant,
