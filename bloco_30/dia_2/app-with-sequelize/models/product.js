47aa3a288f4bf398949691f0dbf2e723fdafd87f
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-24 00:05:51 -0300

    adiciona seed

--- bloco_30/dia_2/app-with-sequelize/models/product.js
+++ bloco_30/dia_2/app-with-sequelize/models/product.js
@@ -2,6 +2,7 @@ const Product = (sequelize, DataTypes) => {
   const Product = sequelize.define("Product", {
     name: DataTypes.STRING,
     price: DataTypes.FLOAT,
+    quantity: DataTypes.INTEGER,
   });
 
   return Product;
