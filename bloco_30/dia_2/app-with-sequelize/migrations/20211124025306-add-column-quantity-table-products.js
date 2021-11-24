140aba2bb19a7f478da1ba9ef69308bd3dacccb7
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-23 23:56:06 -0300

    edita migration gerada

--- bloco_30/dia_2/app-with-sequelize/migrations/20211124025306-add-column-quantity-table-products.js
+++ bloco_30/dia_2/app-with-sequelize/migrations/20211124025306-add-column-quantity-table-products.js
@@ -2,20 +2,12 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    await queryInterface.addColumn('Products', 'quantity', {
+      type: Sequelize.INT,
+    });
+   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn('Products', 'quantity');
   }
 };
